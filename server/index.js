58ecb7ac5aa47c934241bef9beed6549b86e3cbf
Jordan Harlen <jordan_harlen@hotmail.com>
2023-03-25 17:12:12 +1300

    changed port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
 }
 
 const server = require('./server')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
